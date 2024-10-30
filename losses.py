0df38572dd70e23f9f567886f0ebe51d8010a00a
stmugisha <mstevejean@gmail.com>
2024-10-31 00:27:06 +0300

    Add loss and activation derivatives, add mlp with backpropagation

--- losses.py
+++ losses.py
@@ -5,10 +5,10 @@ from typing import List, Union
 import numpy as np
 
 
-def mean_squared_error(
-	true_values: List[Union[float, int]], 
-	predicted_values: List[Union[float, int]]
-	):
+def mse(
+	true_values: List[float], 
+	predicted_values: List[float]
+):
 	"""Mean squared error loss function.
 
 	Defined as the average of the sum of the square differences between
@@ -27,3 +27,24 @@ def mean_squared_error(
 	else: # list
 		return sum([(t - p) ** 2 for t, p in zip(true_values, predicted_values)]) / len(true_values)
 
+
+def mse_delta(
+	true_values: np.ndarray, 
+	predicted_values: np.ndarray
+):
+	"""Derivative of the mean squared error function.
+	The derivative is taken with respect to the predicted value.
+	Therefore, if t is the true and predicted values,
+	differentiating (t - p)^2 by chain rule yields 2*(-1)*(t - p)
+
+	Args:
+		Numpy arrays of the true and predicted values.
+	"""
+	return np.nanmean(-2 * (true_values - predicted_values))
+	# or return np.nanmean(2 * (predicted_values - true_values))
+
+
+def bce_loss():
+	"""Binary Cross Entropy Loss function.
+	"""
+	pass
